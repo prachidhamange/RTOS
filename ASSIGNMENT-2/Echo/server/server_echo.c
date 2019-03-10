0660091eeccac85ef9bbba745311cfb48909812c
prachidhamange <47719215+prachidhamange@users.noreply.github.com>
2019-03-11 00:46:40 +0530

    Update server_echo.c

--- ASSIGNMENT-2/Echo/server/server_echo.c
+++ ASSIGNMENT-2/Echo/server/server_echo.c
@@ -1,16 +1,14 @@
 
-//Example code: A simple server side code, which echos back the received message. 
-//Handle multiple socket connections with select and fd_set on Linux  
 #include <stdio.h>  
-#include <string.h>   //strlen  
+#include <string.h>     
 #include <stdlib.h>  
 #include <errno.h>  
-#include <unistd.h>   //close  
-#include <arpa/inet.h>    //close  
+#include <unistd.h>    
+#include <arpa/inet.h>     
 #include <sys/types.h>  
 #include <sys/socket.h>  
 #include <netinet/in.h>  
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros  
+#include <sys/time.h> 
      
 #define TRUE   1  
 #define FALSE  0  
@@ -55,9 +53,9 @@ int main(int argc , char *argv[])
     }   
      
     //type of socket created  
-    address.sin_family = AF_INET;   
-    address.sin_addr.s_addr = INADDR_ANY;   
-    address.sin_port = htons( PORT );   
+    address.sin_family = AF_INET;  //this allows connection across computers 
+    address.sin_addr.s_addr = INADDR_ANY;//this return the address of server itself   
+    address.sin_port = htons( PORT );  //htons converts PORT number to network byte order format  
          
     //bind the socket to localhost port 8888  
     if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)   
@@ -102,8 +100,8 @@ int main(int argc , char *argv[])
                 max_sd = sd;   
         }   
      
-        //wait for an activity on one of the sockets , timeout is NULL ,  
-        //so wait indefinitely  
+        //wait indefinitely for an activity on one of the sockets , timeout is NULL ,  
+        
         activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   
        
         if ((activity < 0) && (errno!=EINTR))   
@@ -111,9 +109,7 @@ int main(int argc , char *argv[])
             printf("select error");   
         }   
              
-        //If something happened on the master socket ,  
-        //then its an incoming connection 
-	//while(!FD_ISSET(master_socket, &readfds)) ;
+        //if an incoming connection detected
         if (FD_ISSET(master_socket, &readfds))   
         {   
             if ((new_socket = accept(master_socket,  
@@ -124,17 +120,9 @@ int main(int argc , char *argv[])
             }   
              
             //inform user of socket number - used in send and receive commands  
-            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs 
+            printf("New connection ...., socket fd : %d ,IP : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs 
                   (address.sin_port));   
            
-            //send new connection greeting message  
-            /*if( send(new_socket, message, strlen(message), 0) != strlen(message) )   
-            {   
-                perror("send");   
-            }   
-                 
-            puts("Welcome message sent successfully");  */ 
-                 
             //add new socket to array of sockets  
             for (i = 0; i < max_clients; i++)   
             {   
@@ -149,15 +137,13 @@ int main(int argc , char *argv[])
             }   
         }   
              
-        //else its some IO operation on some other socket 
         for (i = 0; i < max_clients; i++)   
         {   
             sd = client_socket[i];   
                  
             if (FD_ISSET( sd , &readfds))   
             {   
-                //Check if it was for closing , and also read the  
-                //incoming message  
+                //Check if it was for closing , and also read the incoming message  
                 if ((valread = read( sd , buffer, 1024)) == 0)   
                 {   
                     //Somebody disconnected , get his details and print  
