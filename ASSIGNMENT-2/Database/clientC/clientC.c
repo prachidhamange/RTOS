a779cccde009772dbdbb912737a1f087d20929c1
prachidhamange <47719215+prachidhamange@users.noreply.github.com>
2019-05-07 18:27:36 +0530

    Update clientC.c

--- ASSIGNMENT-2/Database/clientC/clientC.c
+++ ASSIGNMENT-2/Database/clientC/clientC.c
@@ -49,7 +49,7 @@ int main(int argc, char const *argv[])
           */
 	
 	//comment the below section while testing individually
-		hello="GET c.txt";
+		strcpy(hello,"GET c.txt");//updated
 		send(sock , &hello , 1 , 0 ); 
 	    	while((valread = read( sock , buffer, 1024))==0); 
 	    	printf("value received: %s\n",buffer ); 
